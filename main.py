be9f7449f29082896782a24e7d42d9006cf7757a
Manthan007 <manthansharma7818@gmail.com>
2026-07-20 00:00:21 -0700

    main updated

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from SIDD.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from SIDD import logger
 from SIDD.pipeline.stage_02_model_training import ModelTrainingPipeline
 
-# STAGE_NAME = "data ingestion stage"
+STAGE_NAME = "data ingestion stage"
 
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
-#     obj = DataIngestionTrainingPipeline()
-#     obj.main()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<")
-# except Exception as e:
-#     raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    raise e
 
 
 STAGE_NAME = "Model Training stage"
